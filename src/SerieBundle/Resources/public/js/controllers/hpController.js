1072eadc49ef8a9f96d6e7017485b789a64b6bc5
MrDrannoc <poyer.k@gmail.com>
2016-12-12 09:46:37 +0100

    hp slider presque fonctionnel

--- src/SerieBundle/Resources/public/js/controllers/hpController.js
+++ src/SerieBundle/Resources/public/js/controllers/hpController.js
@@ -1,56 +1,39 @@
-function hpController() {
+function hpController(tmdbService) {
 
+    this.tmdbService = tmdbService;
+
+    this.load = () => {
+        this.tmdbService.popular().then((response) => {
+            this.results = response.data.results;
+            this.slickCurrentIndex = 0;
+            this.slickConfig = {
+              // dots: true,
+              // initialSlide: 0,
+              slidesToShow: 3,
+              // infinite: true,
+              // centerMode: true,
+              // variableWidth: true,
+              // method: {},
+              event: {
+                afterChange: function(event, slick, currentSlide, nextSlide) {
+                  this.slickCurrentIndex = currentSlide;
+                  console.log(this.slickCurrentIndex);
+                }
+              }
+            };
+        });
+    };
+
+    this.load();
 
     this.show = false;
-    this.tvShows = [{
-        'id': '001',
-        'img': 'bundles/serie/assets/img/2176.png'
-    }, {
-        'id': '002',
-        'img': 'bundles/serie/assets/img/dexter.jpg'
-    }, {
-        'id': '003',
-        'img': 'bundles/serie/assets/img/Breaking-Bad.jpg'
-    }, {
-        'id': '004',
-        'img': 'bundles/serie/assets/img/hannibal.jpg'
-    }, {
-        'id': '005',
-        'img': 'bundles/serie/assets/img/got.jpg'
-    }, {
-        'id': '006',
-        'img': 'bundles/serie/assets/img/walking-dead-ddotomen.jpeg'
-    }, {
-        'id': '007',
-        'img': 'bundles/serie/assets/img/bigbang.jpg'
-    }];
-
-    $(document).ready(setTimeout(function() {
-        $('.multi-item-carousel1').carousel({
-            interval: false
-        });
-        $('.multi-item-carousel1 .item').each(function() {
-            var next = $(this).next();
-            if (!next.length) {
-                next = $(this).siblings(':first');
-            }
-            next.children(':first-child').clone().appendTo($(this));
-
-            if (next.next().length > 0) {
-                next.next().children(':first-child').clone().appendTo($(this));
-            } else {
-                $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-            }
-        });
-    }), 1000);
-
 
     this.isToggled1 = false;
     this.toggleMore = () => {
         this.isToggled1 = !this.isToggled1;
-    }
+    };
     if (this.isToggled1 === true) {
-      $('.card-block').show = true;
+        $('.card-block').show = true;
     }
 
     this.isToggled = false;

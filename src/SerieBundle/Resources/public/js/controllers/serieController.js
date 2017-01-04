c015a79742f5c9d6074225a4014230227d28597f
MrDrannoc <poyer.k@gmail.com>
2017-01-04 15:49:42 +0100

    check des épisodes de la page série + modal + VF

--- src/SerieBundle/Resources/public/js/controllers/serieController.js
+++ src/SerieBundle/Resources/public/js/controllers/serieController.js
@@ -5,46 +5,46 @@ function serieController(tmdbService, $routeParams, $location) {
     this.$location = $location;
 
     this.getSheetSerie = (id) => {
-      this.tmdbService.sheetSerie(id).then((response) => {
-        this.sheetSerie = response.data;
-        console.log(response.data);
-      });
-      this.tmdbService.seasons(id, 1).then((response) => {
-        this.seasons = response.data;
-        console.log(this.seasons);
+        this.tmdbService.sheetSerie(id).then((response) => {
+            this.sheetSerie = response.data;
+            console.log(response.data);
+        });
+        this.tmdbService.seasons(id, 1).then((response) => {
+            this.seasons = response.data;
+            console.log(this.seasons);
 
-      });
+        });
     };
 
     this.getSheetSerie($routeParams.id);
 
     this.getPeople = (id) => {
-      this.tmdbService.people(id).then((response) => {
-        this.people = response.data;
-      });
+        this.tmdbService.people(id).then((response) => {
+            this.people = response.data;
+        });
     };
 
     this.getPeople($routeParams.id);
 
     this.getSeasons = (id, season) => {
-      this.tmdbService.seasons(id, season).then((response) => {
-        this.seasons = response.data;
-      });
+        this.tmdbService.seasons(id, season).then((response) => {
+            this.seasons = response.data;
+        });
     };
 
 
-    this.isToggled = false;
-    $('#star').hide();
-    this.toggleFollow = () => {
-        this.isToggled = !this.isToggled;
-        console.log(this.isToggled);
-        if (this.isToggled === false) {
-            $('#star').hide();
-            $('#star-empty').show();
-        } else {
-            $('#star-empty').hide();
-            $('#star').show();
+    this.episodeTrack = [];
+    this.check = (id) => {
+        if (!this.episodeTrack[id]) {
+            this.episodeTrack[id] = false;
         }
+        this.episodeTrack[id] = !this.episodeTrack[id];
+        console.log(this.episodeTrack);
     };
+
+    this.pourcentage = 50;
+    this.circle = "c100 p" + this.pourcentage + " orange";
+
+
 
 }

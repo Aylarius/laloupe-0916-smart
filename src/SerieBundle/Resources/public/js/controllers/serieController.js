3519a2361b692787607ac073cadde4f279b3c245
Thifaine <thifaine.noirault@gmail.com>
2017-01-16 17:39:30 +0100

    get last watched on modif

--- src/SerieBundle/Resources/public/js/controllers/serieController.js
+++ src/SerieBundle/Resources/public/js/controllers/serieController.js
@@ -126,12 +126,14 @@ function serieController(serieService, episodeService, sessionFactory, tmdbServi
             this.loginMessage.title = "Vous avez bien ajouté cette série à vos séries favorites !";
             this.loginMessage.message = "En cours de redirection...";
             this.getAllWatched($routeParams.id, this.sessionFactory.user.id);
+            this.getLastWatched($routeParams.id, this.sessionFactory.user.id);
         }).catch((res) => {
             this.loginMessage = {};
             this.loginMessage.type = "error";
             this.loginMessage.title = "Erreur lors du suivi";
             this.loginMessage.message = res.data;
             this.getAllWatched($routeParams.id, this.sessionFactory.user.id);
+            this.getLastWatched($routeParams.id, this.sessionFactory.user.id);
         });
     };
 
